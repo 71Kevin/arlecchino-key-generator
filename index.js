2d6ea7bfe11bb3dca086f720ba5fc1fc778579fe
Kevin Oliveira Manhas <kevin.manhas@riobranco.com.br>
2024-05-28 15:15:29 -0300

    Updated version to 1.0.1 with new features and tests

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const keyModel = new KeyModel();
 const keyService = new KeyService(keyModel);
 const keyController = new KeyController(keyService);
 
-module.exports = keyController.generateKey();
+module.exports = keyController.generateKey.bind(keyController);
